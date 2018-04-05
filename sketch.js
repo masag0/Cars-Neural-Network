d1dc81e96a2ec92103ff6b8687cf1bb3c38af90b
Edwin Shin <edwinshin@gmail.com>
2018-04-05 16:03:28 -0700

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  // put setup code here
+  createCanvas(800,800);
+}
+
+function draw() {
+  // put drawing code here
+  background(0,0,0);
+  fill(255);
+  // noFill();
+  stroke(255);
+  ellipse(400,400,600,700);
+  fill(0);
+  ellipse(400,400,350,450);
+  rect(50,50,20,20);
+  // rect(50,50,200,100);
+  // rect(250,50,200,100);
+  // rect(450,50,200,100);
+  // rect(450,50,200,100);
+}
